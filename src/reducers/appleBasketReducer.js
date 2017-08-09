fb26c308214cca5498083d4675465a64d8ae427b
blzdvip <8534353@qq.com>
2017-08-09 15:40:52 +0800

    去掉immutable处理数据，使用Object.assgin()

--- src/reducers/appleBasketReducer.js
+++ src/reducers/appleBasketReducer.js
@@ -1,4 +1,5 @@
 import { fromJS } from 'immutable';
+
 const initialState = {
     isPicking: false,
     newAppleId: 3,
@@ -22,35 +23,47 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
-    
+
     switch (action.type) {
         case 'apple/BEGIN_PICK_APPLE':
-            //将isPicking设置为true
-            return fromJS(state).set('isPicking', true).toJS();
+            //return fromJS(state).set('isPicking', ture).toJS();
+            return Object.assign({}, state, {isPicking: true});
         case 'apple/DONE_PICK_APPLE':
-        
             let newApple = {
                 id: state.newAppleId,
                 weight: action.payload,
                 isEaten: false
-
-            };
-
+            }
             //在apples中新增一个newApple， 将newAppleId增加1， 将isPicking设为false
-            return fromJS(state).update('apples', list =>list.push(newApple))
-                                .set('newAppleId', state.newAppleId + 1)
-                                .set('isPicking', false)
-                                .toJS();
+            // return fromJS(state).update('apples', list => list.push(newApple))
+            //                     .set('newAppleId', state.newAppleId +1)
+            //                     .set('isPicking', false)
+            //                     .toJS();
 
+            return Object.assign({}, state, {
+                apples: [
+                    ...state.apples,
+                    newApple
+                ],
+                newAppleId: state.newAppleId +1,
+                isPicking: false
+            });
         case 'apple/FAIL_PICK_APPLE':
-            //将isPicking设为false
-            return fromJS(state).set('isPicking', false).toJS();
-
+            //return fromJS(state).set('isPicking', false).toJS();
+            return Object.assign({}, state, {isPicking: true});
         case 'apple/EAT_APPLE':
             // 将id对应的索引值的数组的isEaten设为true， 表示已吃
-            return fromJS(state).setIn(['apples', action.payload, 'isEaten'], true).toJS();
-
+            //return fromJS(state).setIn(['apples', action.payload, 'isEaten'], true).toJS();
+            return Object.assign({}, state, {
+                apples: [
+                    ...state.apples.slice(0,action.payload),
+                    Object.assign({}, state.apples[action.payload], {
+                        isEaten: true
+                    }),
+                    ...state.apples.slice(action.payload + 1)
+                ]
+            })
         default:
             return state;
     }
-}
+};
